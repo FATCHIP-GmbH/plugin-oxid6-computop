f0b9114ff583d4d0493953131cc76f58d4a940f2
Robert Müller <robert.mueller@fatchip.de>
2026-09-25 16:31:23 +0200

    FC#0138127 - Added cronjob for finalizing orders (#14)
    * FC#0138127 - Added cronjob for finalizing orders
    * 0138127 fix problem with remaining basket, after paid order but no successful redirect
    
    ---------
    
    Co-authored-by: evollrath <96vlocky96@gmail.com>

--- views/admin/en/fatchip_computop_payments_en_lang.php
+++ views/admin/en/fatchip_computop_payments_en_lang.php
@@ -5,6 +5,7 @@ $aLang = [
     'charset'                                               => 'UTF-8',
 
     'SHOP_MODULE_GROUP_COMPUTOP_GENERAL'                    => 'General',
+    'SHOP_MODULE_GROUP_COMPUTOP_CRONJOBS'                   => 'Cronjobs',
     'SHOP_MODULE_GROUP_COMPUTOP_CREDITCARD'                 => 'Credit Card',
     'SHOP_MODULE_GROUP_COMPUTOP_PAYPAL'                     => 'PayPal',
     'SHOP_MODULE_GROUP_COMPUTOP_PAYPALEXPRESS'              => 'PayPal Express',
@@ -27,6 +28,9 @@ $aLang = [
     'SHOP_MODULE_encryption_aes'                            => 'AES encryption',
     'SHOP_MODULE_refnr_prefix'                              => 'Reference Number Prefix',
     'SHOP_MODULE_refnr_suffix'                              => 'Reference Number Suffix',
+
+    'SHOP_MODULE_cronFinalizeOrdersActive'                    => 'Cronjob "Finalizing of paid/authorized but not completed orders" active',
+    'SHOP_MODULE_cronFinalizeOrdersDays'                      => 'Time window during which attempts are made to finalize paid/authorized but not completed orders',
 
     'SHOP_MODULE_creditCardMode'                            => 'Credit Card - Mode',
     'SHOP_MODULE_creditCardMode_IFRAME'                     => 'IFrame',
@@ -89,11 +93,12 @@ $aLang = [
 
     'SHOP_MODULE_ratepayDirectDebitRequestBic'              => 'Request BIC',
 
-    'HELP_SHOP_MODULE_blMollieShowIcons'                    => 'Your Merchant Id (username)',
     'HELP_SHOP_MODULE_mac'                                  => 'Your HMAC key',
     'HELP_SHOP_MODULE_blowfishPassword'                     => 'Your encryption password',
     'HELP_SHOP_MODULE_debuglog'                             => 'Creates a log file "FatchipCTPayment_.log" with debug output in the Oxid log directory.',
     'HELP_SHOP_MODULE_encryption'                           => 'Type of encryption used.<br>Blowfish encryption is set up as standard by Computop Support.<br>If Blowfish encryption (bf-cbc) is not available on your host, please contact Computop Support to have AES enabled.<br>If AES has been activated by Computop, switch to AES.',
+
+    'HELP_SHOP_MODULE_cronFinalizeOrdersActive'               => 'This cronjob has the job to finalize orders where the customer paid successfully but seemingly didnt return to the shop to complete the order process. The cronjob only finalizes orders within the time period configured in the setting "Time window during which attempts are made to finalize paid/authorized but not completed orders".<br><br>For this cronjob to work, in addition to this checkbox you have to ensure that the cronjob is set up properly. You can find information on how to set up the cronjob in the README.md of this module.',
 
     'HELP_SHOP_MODULE_creditCardMode'                       => '<b>IFrame</b>: The creditcard form will be displayed after clicking "confirm payment" in an iframe<BR><b>Silent Mode</b>: The creditcard form will be displayed on the "complete order" page.<BR><b>Payment Page</b>: Credit card details are entered in a blank page after clicking on "Order payment".',
     'HELP_SHOP_MODULE_creditCardCaption'                    => '<b>AUTO</b>: Reserved amounts will be captured automatically.<BR><b>MANUAL</b>: Reserverd amounts have to be captured manuelly in the shop backend.</p>',
@@ -170,4 +175,6 @@ $aLang = [
     'FATCHIP_COMPUTOP_REFUND' => 'Refund:',
     'FATCHIP_COMPUTOP_REFUND_ALL' => 'Full Refund:',
     'FATCHIP_COMPUTOP_PAYMENTS_APILOG_MAIN' => 'API Log Details',
+    'FATCHIP_COMPUTOP_DAY' => 'day',
+    'FATCHIP_COMPUTOP_DAYS' => 'days',
 ];
